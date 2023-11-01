c79cf0a79f08dfa3deb384eb78621b37563ba6a6
samakhraim <62477773+samakhraim@users.noreply.github.com>
2023-11-01 12:55:14 +0200

    small edits

--- Company/src/Main.java
+++ Company/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         User staff1 = new Staff(11820, "Sama", 15, 0.55, 8);
         User staff2 = new Staff(1, "SS", 15, 0.55, 8);
-        User staff3 = new Staff(2, "John", 30, 0.65, 6);
+        User staff3 = new Staff(2, "sami", 30, 0.65, 6);
         User manager = new Manager(6, "Yaman", 35, 4.5, 8);
         User director = new Director(7, "Hind", 40, 6.2, 7);
         DataBase staffData = new DataBase();
